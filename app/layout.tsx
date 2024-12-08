0bb25797123e6eaac9ae6b5146b46a96b16e0b84
Jenny <jenny@tltc.biz>
2024-12-07 22:01:41 -0800

    removed keys and tokens from published build

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const inter = Inter({
 
 function Footer() {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
+    <footer className="bg-accent-1 border-t border-accent-2 bg-color-1">
       <div className="container mx-auto px-5">
         <div className="py-28 flex flex-col lg:flex-row items-center">
           <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
@@ -26,7 +26,7 @@ function Footer() {
               href="https://nextjs.org/docs"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              Read Nuxt.js Documentation
             </a>
             <a
               href={`https://github.com/web-warrior/contentfulblogdemo`}
