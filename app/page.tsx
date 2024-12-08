1b900b75b29c6c1962782629728c9a5b7db54af4
Jenny <jenny@tltc.biz>
2024-12-07 21:26:39 -0800

    updated home page with some original content, added my name etc.

--- app/page.tsx
+++ app/page.tsx
@@ -13,10 +13,10 @@ function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Jenny McClaine's Contentful Blog Demo.
       </h1>
       <h2 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
+        A statically generated blog example ("statically generated pages" are precompiled vs. being compiled on the fly when making a page request from the browser client, precompiling them makes them load faster for optimized performance and less server side resource overhead) using{" "}
         <a
           href="https://nextjs.org/"
           className="underline hover:text-success duration-200 transition-colors"
