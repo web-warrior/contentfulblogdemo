e5f7639162fe2cc0769e56293fbad0be1b7e9a12
Jenny <jenny@tltc.biz>
2024-12-09 10:15:24 -0800

    line height and new css override

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ function Footer() {
     <footer className="bg-accent-1 border-t border-accent-2 bg-color-1">
       <div className="container mx-auto px-5">
         <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Built with Next.js.
+          <h3 className="footer-h3 text-3xl lg:text-5xl font-bold text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2 tracking-tighter">
+            Built with Next.js & Contentful CMS.
           </h3>
           <div className="flex flex-col justify-center items-center lg:pl-4 lg:w-1/2">
             <a
